a05e534c04106aef2949fa77f7edcec2962093c1
hotqin888 <xc-qin@163.com>
2025-10-18 18:11:42 +0800

    fix bug of "forgot import mysql"

--- doc.go
+++ doc.go
@@ -34,9 +34,9 @@ var blobsDir string
 func init() {
 	f := log.Flags()
 	log.SetFlags(f | log.Lmicroseconds | log.Lshortfile)
-	driver, connStr := "mysql", "travis@/flow?charset=utf8&parseTime=true"
-	tdb := fatal1(sql.Open(driver, connStr)).(*sql.DB)
-	RegisterDB(tdb)
+	// driver, connStr := "mysql", "travis@/flow?charset=utf8&parseTime=true"
+	// tdb := fatal1(sql.Open(driver, connStr)).(*sql.DB)
+	// RegisterDB(tdb)
 }
 
 // RegisterDB provides an already initialised database handle to `flow`.
